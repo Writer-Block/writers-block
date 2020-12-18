751fb1c96eee8b8c75479d40a3156ac388365db0
Noah Pack <noahpack@noahs-mbp.lan>
2020-12-18 10:57:38 -0700

    Set up getUser in reducer and started register component

---
+++ src/redux/reducer.js
@@ -0,0 +1,24 @@
+const initialState = {
+    user: {}
+}
+//Action Types
+const GET_USER = "GET_USER" //Using in the Register page
+
+export const getUser = (user) => {
+    return {
+        type: GET_USER,
+        payload: user
+    }
+}
+
+export default function (state = initialState, action){
+    switch(action.type){
+        case GET_USER:
+            return {
+                ...state,
+                user: action.payload
+            }
+            default:
+                return state
+    }
+}
