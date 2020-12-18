f2002a818d3b3c1c4baf920634092f5c7e029b97
Noah Pack <noahpack@noahs-mbp.lan>
2020-12-18 13:55:04 -0700

    Finished the Register component

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -25,6 +25,51 @@ class Register extends Component{
     handlePassword = (e) => {
         this.setState({ password: e.target.value})
     }
+
+    registerUser = async () => {
+        const {email, username, password} =this.state;
+        try {
+            const user = await axios.post('auth/register', {email, username, password})
+            this.props.getUser(user.data.id, user.data.email, user.data.username)
+            this.props.history.push('/dash')
+        }catch(err){
+        alert(err.response.request.response)
+        }
+    }
+    goToLogin = async () => {
+        this.props.history.push('/login')
+    }
+
+    render(){
+        return (
+            <div className='register'>
+                <form>
+                    <input 
+                        name= 'Email'
+                        value= {this.email}
+                        placeholder='Email'
+                        onChange={this.handleEmail}
+                        />
+                    <input 
+                        name='username'
+                        value={this.email}
+                        placeholder='Username'
+                        onChange={this.handleUsername}
+                        />
+                    <input 
+                        name='password'
+                        type ='password'
+                        value={this.password}
+                        placeholder='Enter Password'
+                        onChange={this.handlePassword}
+                        />
+                        <button onClick={this.registerUser} className='registerBtn'>Register</button>
+                        <button onClick={this.goToLogin}>Already have an account?</button>
+                </form>
+            </div>
+        )
+    }
 }
+
 
 export default connect(null, {getUser})(Register)
