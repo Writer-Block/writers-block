a39892dc4cf8c94e4c28bde162fbc6f30d661acf
Alex <alexbean95@gmail.com>
2020-12-28 16:14:17 -0700

    working S3

--- server/controllers/picController.js
+++ server/controllers/picController.js
@@ -1,8 +1,81 @@
+require('dotenv').config()
+const aws = require("aws-sdk")
+const {AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, S3_BUCKET} = process.env
+
 module.exports = {
     getProfilePic: async (req, res) => {
         const db = req.app.get('db')
         const {user_id} = req.params
         const profilePic = await db.picture.get_pic([user_id])
         res.status(200).send(profilePic[0].profile_pic)
+    },
+
+    config: (req, res) => {
+        aws.config = {
+            region: "us-west-2",
+            accessKeyId: AWS_ACCESS_KEY_ID,
+            secretAccessKey: AWS_SECRET_ACCESS_KEY
+        }
+
+        const s3 = new aws.S3()
+        const fileName = req.query["file-name"]
+        const fileType = req.query["file-type"]
+        const s3Params = {
+            Bucket: S3_BUCKET,
+            Key: fileName,
+            Expires: 60,
+            ContentType: fileType,
+            ACL: "public-read"
+        }
+
+        s3.getSignedUrl("putObject", s3Params, (err, data) => {
+            if (err) {
+                console.log(err)
+                return res.end()
+            }
+            const returnData = {
+                signedRequest: data,
+                url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+            }
+
+            return res.send(returnData)
+        })
+    },
+    deleteProfilePic: (req, res) => {
+       
+        aws.config = {
+          region: "us-west-2",
+          accessKeyId: AWS_ACCESS_KEY_ID,
+          secretAccessKey: AWS_SECRET_ACCESS_KEY,
+        };
+  
+        const {picUrl} = req.body
+
+        const fileName = picUrl.replace(
+            `https://${S3_BUCKET}.s3.amazonaws.com/`, ""
+          )
+
+        const s3 = new aws.S3();
+        const params = {
+          Bucket: S3_BUCKET,
+          Key: fileName
+        }
+        s3.deleteObject(params, (err, data) => {
+          if (err){
+            console.log(err, err.stack)
+          } 
+          else{
+            console.log("Sucess deleting from bucket")
+          }
+        })
+    },
+
+    updateProfilePic: async (req, res) => {
+        const db = req.app.get("db")
+        const {user_id, url} = req.body
+        let update = await db.auth.update_profile_pic(+user_id, url)
+        update = update[0]
+        res.status(200).send(update)
     }
+
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,19 @@ import {Link, useHistory} from "react-router-dom"
 import {connect, useDispatch} from "react-redux"
 import {logoutUser} from "../../redux/reducer"
 import axios from "axios"
+import { v4 as randomString } from 'uuid'
+import {useDropzone} from 'react-dropzone'
+import { GridLoader } from 'react-spinners'
 import "./Header.css"
 
 const Header = (props) => {
 
+    const {user_id} = props
     const history = useHistory()
     const dispatch = useDispatch()
     const [pic, setPic] = useState("")
+    const [update, setUpdate] = useState(false)
+    const [isUploading, setUploading] = useState(false)
 
     const logout = async () => {
         try {
@@ -24,18 +30,78 @@ const Header = (props) => {
     useEffect(() =>{
         const getPic = async () => {
             try {
-                const res = await axios.get(`/api/pic/${props.user_id}`)
+                const res = await axios.get(`/api/pic/${user_id}`)
                 setPic(res.data)
               } catch (err) {
                 console.log(err)
               }
           }
           getPic()
-    }, [props.user_id])
+    }, [user_id])
+
+    const getSignedReq = ([file]) => {
+        setUploading(true)
+        
+        const fileName = `${randomString()}-${file.name.replace(/\s/g, '-')}`
+        axios.get('/api/signs3', {
+            params: {
+                'file-name': fileName,
+                'file-type': file.type
+            }
+        }).then(res => {
+            const {signedRequest, url} = res.data
+            uploadFile(file, signedRequest, url)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    const uploadFile = (file, signedRequest, url) => {
+        const options = {
+            headers: {
+                'Content-Type': file.type
+            }
+        }
+            
+        deleteProfilePic()
+
+
+        axios.put(signedRequest, file, options).then(() => {
+            setPic(url)
+            axios.put("/api/user", {user_id, url}).then((res) => {
+                setUpdate(false)
+            })
+            setUploading(false)
+        }).catch(err => {
+            setUploading(false)
+            if (err.response.status === 403){
+                alert(`Your request for a signed URL failed with a status 403. Double check the CORS config and bucket policy in Matts repo.\n${err.stack}`)
+            } else {
+                alert(`Error: ${err.status}\n ${err.stack}`)
+            }
+        })
+    }
+
+    const deleteProfilePic = () => {
+        try {
+            axios.post("/api/signs3", {pic: pic})
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: "image/*",
+        multiple: false,
+        onDrop: (file) => {
+            getSignedReq(file)
+        }
+    })
+
 
         return (
             <div>
-                {props.user_id
+                {user_id
                 ?
                 <div className = "header">
                     <Link to = "/createpost"
@@ -64,6 +130,34 @@ const Header = (props) => {
                         Logout
                     </Link>
                     <img className = "profile-pic" alt = "profile-pic" src = {`${pic}`}/>
+                    {update
+                            ?
+                            <div className = "update-box">
+                                <div {...getRootProps({className: "drop-zone"})}>
+                                    <input {...getInputProps()} />
+                                    {isUploading 
+                                    ? 
+                                        <GridLoader/> 
+                                    : 
+                                        <h3>Drop File or Click Here</h3>}
+                                </div>
+                                <button
+                                    className = "cancel-button"
+                                    onClick = {() => {
+                                        setUpdate(!update)
+                                    }}> 
+                                    Cancel
+                                </button> 
+                            </div>
+                            :
+                            <button
+                                className = "update-button"
+                                onClick = {() => {
+                                    setUpdate(!update)
+                                }}> 
+                                Update Profile Picture 
+                            </button>  
+                        }
                 </div>
                 :
                     null
